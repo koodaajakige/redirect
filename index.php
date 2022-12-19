bb51e7ffec0e7d28e0ae6e0d70612d78fd222f92
Kirsi Nykänen <kirsi.nykanen@edu.sasky.fi>
2022-12-19 15:08:28 +0200

    tietokannan kytkeminen

--- index.php
+++ index.php
@@ -2,12 +2,18 @@
 /* This will give an error. Note the output
  * above, which is before the header() call */
 
-//Luodaan osoitteet-taulukko.
-$osoitteet = array("NG5TG" => "https://www.w3schools.com/php/",
-"R7E7L" =>	"https://www.php.net/manual/en/index.php",
-"S44E8" =>	"https://thevalleyofcode.com/php/",
-"UDCJ9" =>	"https://phpapprentice.com/",
-"ZZU1M" =>	"https://phptherightway.com/");
+// Määritellään tietokantayhteyden muodostamisessa
+// tarvittavat tiedot.
+$dsn = "mysql:host=localhost;" .
+"dbname={$_SERVER['DB_DATABASE']};" .
+"charset=utf8mb4";
+$user = $_SERVER['DB_USERNAME'];
+$pass = $_SERVER['DB_PASSWORD'];
+$options = [
+PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+PDO::ATTR_EMULATE_PREPARES => false,
+];
 
 // Tarkistetaan, onko URL-osoitteessa annettu hash-parametri.
 if (isset($_GET["hash"])) {
@@ -15,27 +21,44 @@ if (isset($_GET["hash"])) {
     // hash-parametrilla on arvo, poimitaan se muuttujaan.
     $hash = $_GET["hash"];
 
-    // Tarkistetaan, onko taulukossa arvoa hash-muuttujan arvolla.
-    if (isset($osoitteet[$hash])) {
+    try {
+        // Avataan tietokantayhteys luomalla PDO-oliosta ilmentymä.
+        $pdo = new PDO($dsn, $user, $pass, $options);
 
-    // Taulukossa on hash-muuttujaa vastaava avain, haetaan osoite.
-    $url = $osoitteet[$hash];
+        // Alustetaan hakukysely.
+        $stmt = $pdo->prepare("SELECT url
+        FROM osoite
+        WHERE tunniste = ?");
 
-    // Edelleenohjataan taulukosta löytyvään osoitteeseen.
-    header("Location: " . $url);
-    exit;
+        // Suoritetaan kysely ja haetaan tuloksen rivi.
+        $stmt->execute([$hash]);
+        $rivi = $stmt->fetch();
+
+        if ($rivi) {
+
+            // Edelleenohjataan riviltä löytyvään osoitteeseen.
+            $url = $rivi['url'];
+            header("Location: " . $url);
+            exit;
+
+        } else {
+
+          // Taulusta ei löytynyt tunnistetta vastaavaa riviä,
+          // tulostetaan virheilmoitus.
+          echo "Väärä tunniste :(";
+
+      }
+      } catch (PDOException $e) {
+        // Avaamisessa tapahtui virhe, tulostetaan virheilmoitus.
+        echo $e->getMessage();
+      }
 
     } else {
-    // Taulukosta ei löytynyt hash-muuttujaa vastaavaa avainta,
-    // tulostetaan virheilmoitus.
-    echo "Väärä tunniste :(";
+
+      // hash-parametrilla ei ole arvoa, tulostetaan käyttäjälle
+      // esittelyteksti.
+      echo "Tämä on osoitteiden lyhentäjä. Odota maltilla, " .
+      "tänne tulee tulevaisuudessa lisää toiminnallisuutta.";
     }
 
-  } else {
-    // hash-parametrilla ei ole arvoa, tulostetaan käyttäjälle
-    // esittelyteksti.
-    echo "Tämä on osoitteiden lyhentäjä. Odota maltilla, " .
-    "tänne tulee tulevaisuudessa lisää toiminnallisuutta.";
-    }
-    
 ?>
